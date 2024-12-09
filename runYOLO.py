e8c62249f8041294f3ae0ee65e0164f8eb858509
Aaron Hales <a02353892@aggies.usu.edu>
2024-12-09 12:17:53 -0700

    changed run file

--- runYOLO.py
+++ runYOLO.py
@@ -3,9 +3,9 @@ from ultralytics import YOLO
 import torch
 
 def train_model():
-    num = 0
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    modelDir = './runs/detect/train4/weights/best.pt'
+    print(f'Will be using {device}')
+    modelDir = 'yolov8n.pt'
     model = YOLO(modelDir)
     imageSize = 640
 
@@ -22,22 +22,6 @@ def train_model():
     metrics = model.val(data='data.yaml',
                         split='test',
                         save_json=True,)
-
-    # run prediction on the test dataset
-    predict = model.predict(source='./data/obj/test')
-    file = open('./runs/detect/train5/predictStuff.csv', 'w')
-    file.write(f'{predict}')
-    file.close()
-
-        # print('---------------- showing ----------------')
-        # # perform obj detection on image
-        # results = model('data/obj/test/2013_07_28_CHBRC_13_14_140_RH_VK.png')
-        # for i in range(len(results)):
-        #     results[i].show()
-        #
-        # results2 = model('data/obj/train/img8616.png')
-        # for i in range(len(results2)):
-        #     results2[i].show()
 
     print('---------------- exporting ----------------')
     # export format
